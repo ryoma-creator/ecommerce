28e9014ef9d45c961269088ee94f1f7a9f59e4e0
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-26 15:35:07 +0800

    feat: Create ProductContext for managing product state and fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,17 @@ const App = () => {
   return (
     <div className='overflow-hidden'>
       <BrowserRouter>
+
         <Routes>
           <Route path='/' element={<Home />} />
            {/* / に対応するルートは、パラメータを必要としないため、特にid指定する必要はありません。 */}
           <Route path='/product/:id' element=
           {<ProductDetails />} />
         </Routes>
+        
+        <Sidebar />
+        <Footer />
+        
       </BrowserRouter>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+// product provider
+import ProductProvider from './contexts/ProductContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <ProductProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </ProductProvider>
 );
