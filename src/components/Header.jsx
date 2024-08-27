7adb382f2b31d68da8d44964435917a46bd80a3c
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-27 19:45:04 +0800

    Added close button to Sidebar.jsx and bag icon to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
 import React, { useContext } from 'react'
+// sidebar context
 import { SidebarContext } from '../contexts/SidebarContext';
+// import icons
+import { BsBag } from 'react-icons/bs';
 
 const Header = () => {
   const {isOpen, setIsOpen} = useContext(SidebarContext)
   return (
     <div>
       <div>Header</div>
-      <div onClick={()=> setIsOpen(!isOpen)}>open/close sidebar</div>
+      <div onClick={()=> setIsOpen(!isOpen)}
+      className='cursor-pointer flex relative'
+      >
+        <BsBag className='text-2xl'/>
+      </div>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -17,7 +17,20 @@ const Sidebar = () => {
       isOpen ? 'right-0' : '-right-full'
   } w-full bg-white fixed top-0 h-full
     shadow-2xl md:w-[35vw] xl:max-w-[30vw] transition-all
-    duration-300 z-20 px-4 lg:px-[35px]`}>Sidebar</div>
+    duration-300 z-20 px-4 lg:px-[35px]`}>
+      <div className='flex items-center justify-between py-6
+      border-b'>
+        {/* icon */}
+        <div className='uppercase text-sm
+        font-semibold'>Shopping Bag (0)</div>
+
+        <div
+         onClick={handleClose}
+         className='cursor-pointer w-8 h-8 center-flex'>
+          <IoMdArrowForward className='text-2xl'/>
+        </div>
+      </div>
+    </div>
   );
 };
 
