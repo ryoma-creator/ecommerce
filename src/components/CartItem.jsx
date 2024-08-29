1c234c8184bd34a5d6f7925a5dc6f9c23fb04cbb
Ryoma/Cross <ryoma111326@gmail.com>
2024-08-29 20:02:15 +0800

    update layout for CartItem...

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -10,7 +10,8 @@ const CartItem = ({ item }) => {
 
 
   return (
-    <div className='flex'>
+    <div className='flex gap-x-4 py-2 lg:px-6 border-b
+    border-gray-200 w-full font-light text-gray-500'>
       <div className='w-full min-h-[150px] flex items-center gap-x-4'>
           {/* image */}
             <Link to={`/product/${id}`}>
@@ -33,10 +34,10 @@ const CartItem = ({ item }) => {
                       />
                     </div>
                 </div>
-                   <div className='bg-pink-200 flex gap-x-2 h-[36px] text-sm'>
+                   <div className='flex gap-x-2 h-[36px] text-sm'>
                       {/* quantity */}
                       <div className='flex flex-1 max-w-[100px]
-                      bg-blue-400 items-center h-full border
+                      items-center h-full border
                       text-primary font-medium'>
                           {/* minus icon */}
                           <div className='flex-1 center-flex cursor-pointer'>
@@ -54,9 +55,15 @@ const CartItem = ({ item }) => {
                       </div>
 
                       {/* item price */}
-                      <div>item price</div>
+                      <div className='flex-1 flex items-center justify-around'>$ {price}</div>
                       {/* final price */}
-                      <div>final price</div>
+                      {/* .toFixed(2) ➡️ make the price at 2 decimal(小数点以下2桁まで表示) */}
+                      {/* parseFloat()➡️ A number with a decimal point(小数点). */}
+                      <div className='flex-1 flex justify-end items-center
+                      text-primary font-medium'>
+                        {`${parseFloat(price * amount).toFixed(
+                        2
+                      )}`}</div>
                   </div>
             </div>
       </div>
