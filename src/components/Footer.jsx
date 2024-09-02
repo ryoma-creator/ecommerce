316d130c4cb73ebc8588fffab06533d81c27e5ed
Ryoma/Cross <ryoma111326@gmail.com>
2024-09-02 13:38:03 +0800

    Add Footer component with copyright information

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-primary py-12 w-full'>
+      <div className='container mx-auto'>
+        <p className='text-white center-flex'>
+          Copyright &copy; E-commerce Shop 2024. All rights
+          reserved.
+        </p>
+      </div>
+    </footer>
   )
 }
 
